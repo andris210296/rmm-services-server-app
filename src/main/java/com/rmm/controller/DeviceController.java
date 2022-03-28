80584a4287765e3416f484e3c5f6de3a2ac0a806
Andris Andermann <andris.andermann@terra.com.br>
2022-03-28 12:05:55 -0300

    Device - read, update and delete
    This commit allows the customer to read, update or delete an specific device

--- src/main/java/com/rmm/controller/DeviceController.java
+++ src/main/java/com/rmm/controller/DeviceController.java
@@ -30,48 +30,60 @@ public class DeviceController {
 	public Device create(@RequestHeader("Authorization") String authorization, @RequestBody Device device) {
 
 		Customer customer = returnCustomerFromToken(authorization);
-		
+
 		device.setCustomer(customer);
 		return deviceRepository.save(device);
 	}
 
 	@GetMapping(value = "/myDevices")
-	public List findByCustomer(@RequestHeader("Authorization") String authorization) {
-		
-		Customer customer = returnCustomerFromToken(authorization);		
-		
+	public List<Device> findByCustomer(@RequestHeader("Authorization") String authorization) {
+
+		Customer customer = returnCustomerFromToken(authorization);
+
 		return deviceRepository.findByCustomer(customer);
 	}
 
-	@GetMapping(path = { "/{id}" })
-	public ResponseEntity findById(@PathVariable long id) {
-		return deviceRepository.findById(id).map(record -> ResponseEntity.ok().body(record))
-				.orElse(ResponseEntity.notFound().build());
+	@GetMapping(path = { "/myDevices/{id}" })
+	public ResponseEntity findById(@RequestHeader("Authorization") String authorization, @PathVariable long id) {
+
+		Customer customer = returnCustomerFromToken(authorization);
+
+		return deviceRepository.findById(id).filter(device -> device.getCustomer().getId() == customer.getId())
+				.map(device -> ResponseEntity.ok().body(device)).orElse(ResponseEntity.notFound().build());
 	}
 
-	@PutMapping(value = "/{id}")
-	public ResponseEntity update(@PathVariable("id") long id, @RequestBody Device device) {
-		return deviceRepository.findById(id).map(record -> {
-			record.setSystemName(device.getSystemName());
-			record.setType(device.getType());
-			Device updated = deviceRepository.save(record);
-			return ResponseEntity.ok().body(updated);
-		}).orElse(ResponseEntity.notFound().build());
+	@PutMapping(value = "/myDevices/{id}")
+	public ResponseEntity update(@RequestHeader("Authorization") String authorization, @PathVariable("id") long id,
+			@RequestBody Device device) {
+
+		Customer customer = returnCustomerFromToken(authorization);
+
+		return deviceRepository.findById(id).filter(record -> record.getCustomer().getId() == customer.getId())
+				.map(record -> {
+					record.setSystemName(device.getSystemName());
+					record.setType(device.getType());
+					Device updated = deviceRepository.save(record);
+					return ResponseEntity.ok().body(updated);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 
-	@DeleteMapping(path = { "/{id}" })
-	public ResponseEntity<?> delete(@PathVariable long id) {
-		return deviceRepository.findById(id).map(record -> {
-			deviceRepository.deleteById(id);
-			return ResponseEntity.ok().build();
-		}).orElse(ResponseEntity.notFound().build());
+	@DeleteMapping(path = { "/myDevices/{id}" })
+	public ResponseEntity<?> delete(@RequestHeader("Authorization") String authorization, @PathVariable long id) {
+
+		Customer customer = returnCustomerFromToken(authorization);
+
+		return deviceRepository.findById(id).filter(record -> record.getCustomer().getId() == customer.getId())
+				.map(record -> {
+					deviceRepository.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	private Customer returnCustomerFromToken(String token) {
-		String jwt = token.substring(7);		
+		String jwt = token.substring(7);
 		String userName = jwtUtil.extractUserName(jwt);
 		return customerRepository.findByUserName(userName)
 				.orElseThrow(() -> new UsernameNotFoundException("Not found" + userName));
-		
+
 	}
 }
